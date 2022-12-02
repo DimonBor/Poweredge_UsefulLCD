6f596cae7666a84b06360a7fadb7e76efc7a9c53
DimonBor <firecat@orangefox.tech>
2022-12-02 17:57:23 +0200

    ASCII indexing fix

--- src/lcd.cpp
+++ src/lcd.cpp
@@ -4,16 +4,18 @@ class LCD { // Class for interaction with LCD
         
         void writeToLCD(std::string textToWrite) {
             std::vector<char> charToWrite(textToWrite.c_str(), textToWrite.c_str() + textToWrite.size() + 1);
-            std::string charRawValues("ipmitool raw 0x6 0x58 193 0x0 0x0");
+            std::string charRawValues("ipmitool raw 0x6 0x58 193 0x0 0x0 0x" + std::to_string(charToWrite.size()));
 
             if (charToWrite.size() == 0 || charToWrite.size() > 14) {
                 std::cout << "Wrong text size!!!" << std::endl;
                 exit(0);
             }
 
-            for (int i = 0; i < charToWrite.size(); i++) 
-                charRawValues += " 0x" + std::to_string(int(charToWrite[i]));
-
+            for (int i = 0; i < charToWrite.size(); i++) {
+                std::stringstream ss;
+                ss << std::hex << int(charToWrite[i]);
+                charRawValues += " 0x" + ss.str();
+            }
             const char* command = charRawValues.c_str();
 
             system(command);
