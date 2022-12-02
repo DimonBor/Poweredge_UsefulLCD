6aae9e0dde66da994c9b170e149f95340d0df1be
DimonBor <firecat@orangefox.tech>
2022-12-02 21:54:17 +0200

    Another try to save 1 character

--- src/lcd.cpp
+++ src/lcd.cpp
@@ -6,12 +6,13 @@ class LCD { // Class for interaction with LCD
             std::vector<char> charToWrite(textToWrite.c_str(), textToWrite.c_str() + textToWrite.size() + 1);
             std::string charRawValues("ipmitool raw 0x6 0x58 193 0x0 0x0 0x" + std::to_string(charToWrite.size()));
 
-            if (charToWrite.size() == 0 || charToWrite.size() > 14) {
+            if (charToWrite.size() == 0 || charToWrite.size() > 15) {
                 std::cout << "Wrong text size!!!" << std::endl;
                 exit(0);
             }
 
             for (int i = 0; i < charToWrite.size(); i++) {
+                if (charToWrite[i] == '\0') break;
                 std::stringstream ss;
                 ss << std::hex << int(charToWrite[i]);
                 charRawValues += " 0x" + ss.str();
